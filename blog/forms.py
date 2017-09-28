cdc3463959ffe5655005e1a2965f6ee52a2a553f
Maseline <mmusimire@gmail.com>
2017-09-28 13:49:00 +0300

    Add post list,details,form and css

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+__author__ = 'Musimire'
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
--- mysite/settings.py
+++ mysite/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Kampala/Uganda'
+TIME_ZONE = 'Africa/Kampala'
 
 USE_I18N = True
 
@@ -101,4 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static') DO NOT ADD THIS LINE
