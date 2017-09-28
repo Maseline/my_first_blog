ff887d33aa0db434b3702535af5e538924134b3c
Maseline <mmusimire@gmail.com>
2017-09-28 13:58:52 +0300

    Add STATIC_ROOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,4 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static') DO NOT ADD THIS LINE
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
